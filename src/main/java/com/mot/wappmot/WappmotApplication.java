f9df7e31fdd8057cfa883f5a626870e2f50fe2f7
giwi97 <giwanthawijekoon@gmail.com>
2020-12-03 01:21:31 +0530

    Project initiated with java

---
+++ src/main/java/com/mot/wappmot/WappmotApplication.java
@@ -0,0 +1,14 @@
+package com.mot.wappmot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WappmotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WappmotApplication.class, args);
+		System.out.println("Hellowwwwwwwwww");
+	}
+
+}
---
+++ src/test/java/com/mot/wappmot/WappmotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mot.wappmot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WappmotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
