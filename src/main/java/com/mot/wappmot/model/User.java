78d0cfdad96372bb06563c133accc570ae49f3b8
giwi97 <giwanthawijekoon@gmail.com>
2020-12-03 19:04:51 +0530

    Model Class created for User

---
+++ src/main/java/com/mot/wappmot/model/User.java
@@ -0,0 +1,81 @@
+package com.mot.wappmot.model;
+
+public class User {
+
+    private int id;
+    private String nic;
+    private String name;
+    private String company;
+    private int mobile;
+    private String city;
+    private String password;
+
+    public User() {
+    }
+
+    public User(int id, String nic, String name, String company, int mobile, String city, String password) {
+        this.id = id;
+        this.nic = nic;
+        this.name = name;
+        this.company = company;
+        this.mobile = mobile;
+        this.city = city;
+        this.password = password;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNic() {
+        return nic;
+    }
+
+    public void setNic(String nic) {
+        this.nic = nic;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCompany() {
+        return company;
+    }
+
+    public void setCompany(String company) {
+        this.company = company;
+    }
+
+    public int getMobile() {
+        return mobile;
+    }
+
+    public void setMobile(int mobile) {
+        this.mobile = mobile;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
