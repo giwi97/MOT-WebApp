3413ddddc806f4dcfd565ae12f17d3e73de22643
giwi97 <giwanthawijekoon@gmail.com>
2020-12-03 19:12:34 +0530

    Initiated Service and Repository for Registration

---
+++ src/main/java/com/mot/wappmot/repository/RegistrationRepository.java
@@ -0,0 +1,10 @@
+package com.mot.wappmot.repository;
+
+import com.mot.wappmot.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RegistrationRepository extends JpaRepository<User, Integer> {
+
+
+
+}
