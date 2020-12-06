e3c5a00ea54fb5c126ebad2facf3e3e44ba36c0f
giwi97 <giwanthawijekoon@gmail.com>
2020-12-07 01:14:24 +0530

    Registration

--- src/main/java/com/mot/wappmot/controller/RegistrationController.java
+++ src/main/java/com/mot/wappmot/controller/RegistrationController.java
@@ -1,10 +1,34 @@
 package com.mot.wappmot.controller;
 
+import com.mot.wappmot.model.User;
+import com.mot.wappmot.service.RegistrationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class RegistrationController {
 
+    @Autowired
+    private RegistrationService regService;
 
+    @GetMapping("/registeruser")
+    public User registerUser(@RequestBody User user){
+
+        String tempNIC = user.getNic();
+
+        if (tempNIC != null && !"".equals(tempNIC)){
+
+
+
+        }
+
+        User userObj = null;
+        userObj = regService.saveUser(user);
+        return userObj;
+
+    }
 
 }
--- src/main/java/com/mot/wappmot/service/RegistrationService.java
+++ src/main/java/com/mot/wappmot/service/RegistrationService.java
@@ -1,7 +1,21 @@
 package com.mot.wappmot.service;
 
+import com.mot.wappmot.model.User;
+import com.mot.wappmot.repository.RegistrationRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
 
 @Service
 public class RegistrationService {
+
+    @Autowired
+    private RegistrationRepository regRepo;
+    
+    public User saveUser(User user){
+
+        return regRepo.save(user);
+
+    }
+
 }
