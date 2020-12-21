11bec7adaa344d06f281338a2e90d862175fd90b
giwi97 <giwanthawijekoon@gmail.com>
2020-12-22 02:23:48 +0530

    Products model modifications

--- src/main/java/com/mot/wappmot/model/Products.java
+++ src/main/java/com/mot/wappmot/model/Products.java
@@ -1,16 +1,18 @@
 package com.mot.wappmot.model;
 
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import java.sql.Date;
+import java.sql.Time;
 
+@Entity
 public class Products {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
-    private String date;
-    private String time;
     private String category;
     private String item;
     private double minPrice;
@@ -19,10 +21,9 @@ public class Products {
     public Products() {
     }
 
-    public Products(int id, String date, String time, String category, String item, double minPrice, double maxPrice) {
+    public Products(int id, String category, String item, double minPrice, double maxPrice) {
+
         this.id = id;
-        this.date = date;
-        this.time = time;
         this.category = category;
         this.item = item;
         this.minPrice = minPrice;
@@ -35,22 +36,6 @@ public class Products {
 
     public void setId(int id) {
         this.id = id;
-    }
-
-    public String getDate() {
-        return date;
-    }
-
-    public void setDate(String date) {
-        this.date = date;
-    }
-
-    public String getTime() {
-        return time;
-    }
-
-    public void setTime(String time) {
-        this.time = time;
     }
 
     public String getCategory() {
