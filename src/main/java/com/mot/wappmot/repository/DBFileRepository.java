a038bb7664c2bf4bcbe5f927dc6cd199d2f4ddf9
giwi97 <giwanthawijekoon@gmail.com>
2020-12-26 02:22:34 +0530

    Repo for file upload

---
+++ src/main/java/com/mot/wappmot/repository/DBFileRepository.java
@@ -0,0 +1,10 @@
+package com.mot.wappmot.repository;
+
+import com.mot.wappmot.model.DBFile;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DBFileRepository extends JpaRepository<DBFile, String> {
+
+
+
+}
