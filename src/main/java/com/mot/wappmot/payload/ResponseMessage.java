7c690b205517b0c1d83e395c4ad5a0feb6df0ab0
giwi97 <giwanthawijekoon@gmail.com>
2020-12-28 03:43:31 +0530

    Response

---
+++ src/main/java/com/mot/wappmot/payload/ResponseMessage.java
@@ -0,0 +1,25 @@
+package com.mot.wappmot.payload;
+
+public class ResponseMessage {
+
+    private String message;
+
+    public ResponseMessage(String message) {
+
+        this.message = message;
+
+    }
+
+    public String getMessage(){
+
+        return message;
+
+    }
+
+    public void setMessage(String message){
+
+        this.message = message;
+
+    }
+
+}
